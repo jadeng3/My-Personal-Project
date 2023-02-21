a016c4c5b430ab266fa6ef19d67c131b58b6a3f6
Janice <jadeng3@s.sfusd.edu>
2023-02-21 11:29:24 -0800

    made it so that player count jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 2.0f;
+    public float jumpForce = 10f; // The force applied to make the player jump
     private Rigidbody playerRb;
+    public float xRange = 10.0f;
+    public float gravityModifier;
 
-    // Start is called before the first frame update
+    public float jumpforce = 10.0f;
+    public int maxJumps = 50; // The maximum number of jumps the player can perform
+    private int jumpCount = 0; // The number of jumps the player has performed
+
+
+    private Rigidbody rb; // The Rigidbody component of the player
+
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         playerRb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < maxJumps)
+        {
+            Jump();
+        }
+        if (transform.position.x < -xRange)
+        { transform.position = new Vector3(-xRange, transform.position.y, transform.position.z); }
+        if (transform.position.x < xRange)
+        { transform.position = new Vector3(xRange, transform.position.y, transform.position.z); }
+    }
 
-        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, jumpForce);
+        jumpCount++;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            jumpCount = 0;
+        }
     }
 }
+
